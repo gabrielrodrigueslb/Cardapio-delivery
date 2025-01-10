1e1f80ae6897e3ccee775b5eaae47da939a31cfe
Gabriel <gbri2586@gmail.com>
2025-01-10 20:47:25 -0300

    Deploy header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import './global.css'
+import { Header } from './assets/Header'
+
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Header.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
+
+export function Header(){
+
+
+    return(
+        <>
+            <div className="bg-top"></div>
+
+            <header className="width-fix mt-5">
+        <div className="card">
+            <div className="d-flex container-header">
+                <div className="container-img">
+                </div>
+
+                <div className="infos">
+                    <h1><b>Pizzaria Maluca</b></h1>
+                    <div className="infos-sub">
+                        <p className="status-open"><FontAwesomeIcon icon={faClock} size='1x'/> Aberta</p>
+                        <a href="#" className="link">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+        </>
+    )
+}
+
+
